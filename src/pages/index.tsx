af8ae440acabf60fabc7473d09979f40f525334c
Martins Pereira <martins.silva@etec.sp.gov.br>
2022-01-25 18:15:03 -0300

    header e home adicionados

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,26 @@
-import styles from '../styles/home.module.scss'
+import styles from './home.module.scss'
+import Head from 'next/head'
+import { SubscribeButton } from '../Components/SubscribeButton'
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>Hello World</h1>
+    <>
+      <Head>
+        <title>Home | Ignews</title>
+      </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>Get acess to all the publication <br />
+            <span>for $9.98 month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl Coding" />
+      </main>
+    </>
   )
 }
