5661a1d72724d59c597eaf54fa09b291be0d2d42
Martins Pereira <martins.silva@etec.sp.gov.br>
2022-02-12 21:33:55 -0300

    colocado o autenticação com github

--- src/Components/SignInButton/index.tsx
+++ src/Components/SignInButton/index.tsx
@@ -1,16 +1,18 @@
 import styles from './styles.module.scss'
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, useSession, signOut } from 'next-auth/react'
+
 
 export function SignInButton() {
-  const isUserLoggedIn = false;
+  const { data: session } = useSession()
 
-  return isUserLoggedIn ? (
-    <button type='button' className={styles.signInButton}><FaGithub color="#04d361" />
-      Martins Pereira
+  return session ? (
+    <button type='button' onClick={() => signOut()} className={styles.signInButton}><FaGithub color="#04d361" />
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>) : (
-    <button type='button' className={styles.signInButton}><FaGithub color="#eba417" />
+    <button type='button' onClick={() => signIn('github')} className={styles.signInButton}><FaGithub color="#eba417" />
       Sign in with Github
     </button>
   )
